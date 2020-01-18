3a7c52cee9d912ef575d68e6065b21c11887ee67
Jeremy Milledge <jez.milledge@gmail.com>
2020-01-19 01:37:10 +1100

    fixed centering of marker on map

--- src/components/location-map.js
+++ src/components/location-map.js
@@ -12,6 +12,9 @@ const StyledMarker = styled.span`
   color: ${colors.secondary};
   text-shadow: 0 0 3px rgba(0, 0, 0, 0.2);
   font-size: 4em;
+  transform: translate(-50%, -50%);
+  display: block;
+  position: absolute;
 `;
 
 const Marker = ({ icon }) => <StyledMarker>{icon}</StyledMarker>;
