3c7dee12a94bc8f99d0068b2a1cc2058de824649
Jeremy Milledge <jez.milledge@gmail.com>
2020-01-19 23:29:35 +1100

    footer sections defined

--- src/components/footer.js
+++ src/components/footer.js
@@ -18,9 +18,17 @@ const FooterContent = styled.div`
   width: 100%;
   margin: 0 auto;
   padding: 4em 0;
-  text-align: center;
   display: grid;
   grid-template-columns: repeat(3, 1fr);
+`;
+
+const LeftFooter = styled.div``;
+
+const CentralFooter = styled.div`
+  text-align: center;
+`;
+const RightFooter = styled.div`
+  text-align: right;
 `;
 
 const Footer = () => {
@@ -29,9 +37,25 @@ const Footer = () => {
     <StyledFooter>
       <FooterContentContainer>
         <FooterContent>
-          <p>Website Links</p>
-          <p>{`©${year} Stephanie Brancatisano`}</p>
-          <p>Australian Registered Architect #10667</p>
+          <LeftFooter>
+            <p>Website Links</p>
+          </LeftFooter>
+          <CentralFooter>
+            <p>{`©${year} Stephanie Brancatisano`}</p>
+          </CentralFooter>
+          <RightFooter>
+            <p>Australian Registered Architect #10667</p>
+            <p>
+              Website by{' '}
+              <a
+                href="https://zej.com.au/"
+                title="To developer's website"
+                target="_blank"
+              >
+                <strong>zej</strong>
+              </a>
+            </p>
+          </RightFooter>
         </FooterContent>
       </FooterContentContainer>
     </StyledFooter>
