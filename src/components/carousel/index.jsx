cba0a1241f98a4608e296d628e97a1b89604eb0e
Jeremy Milledge <jez.milledge@gmail.com>
2021-05-02 15:02:03 +1000

    use better carousel

--- src/components/carousel/index.jsx
+++ src/components/carousel/index.jsx
@@ -1,105 +1,35 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import styled from '@emotion/styled';
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
 
-import CarouselImage, { carouselImageProps } from './image';
-import NavButton from './nav-button';
-import { colors } from '../../utils/constants';
-
-// TODO: load prev and next for speed
-
-const StyledCarousel = styled.section`
-  height: 61.8vh;
-  max-height: 400px;
-  background: pink;
-  position: relative;
-
-  .carousel-nav {
-    height: 100%;
-    top: 0;
-    position: absolute;
-    cursor: pointer;
-    z-index: 500;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    &:hover span {
-      text-shadow: none;
-    }
-    span {
-      display: inline-block;
-      font-weight: bolder;
-      font-size: 28px;
-      color: ${colors.white};
-      padding: 1rem;
-      text-shadow: 2px 2px 2px rgba(0, 0, 0, 0.5);
-      transition: 0.2s all;
-    }
-  }
-`;
+import Image, { carouselImageProps } from './image';
 
 const Carousel = ({ images }) => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const currentImage = images && images[currentIndex].node;
-
-  const moveLeft = () => {
-    if (!images.length) {
-      return;
-    }
-    if (currentIndex === 0) {
-      setCurrentIndex(images.length - 1);
-    } else {
-      setCurrentIndex((curr) => curr - 1);
-    }
-  };
-
-  const moveRight = () => {
-    if (!images.length) {
-      return;
-    }
-    if (currentIndex === images.length - 1) {
-      setCurrentIndex(0);
-    } else {
-      setCurrentIndex((curr) => curr + 1);
-    }
-  };
-
-  const downHandler = (e) => {
-    if (e.key === 'ArrowLeft') {
-      moveLeft();
-    } else if (e.key === 'ArrowRight') {
-      moveRight();
-    }
-  };
-
-  useEffect(() => {
-    window.addEventListener('keydown', downHandler);
-    return () => {
-      window.removeEventListener('keydown', downHandler);
-    };
-  }, []);
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  const items = images.map(({ node }) => <Image {...node} />);
 
   return (
-    <>
-      {images && (
-        <StyledCarousel>
-          <NavButton onClick={moveLeft} direction="left" />
-          <CarouselImage
-            alt={currentImage.description}
-            title={currentImage.title}
-            fluid={currentImage.fluid}
-          />
-          <NavButton onClick={moveRight} direction="right" />
-        </StyledCarousel>
-      )}
-    </>
+    <div style={{ backgroundColor: 'white' }}>
+      <AliceCarousel
+        autoPlayStrategy="action"
+        items={items}
+        autoPlay
+        autoPlayInterval={1000}
+        keyboardNavigation
+        mouseTracking
+        infinite
+      />
+    </div>
   );
 };
 
 Carousel.propTypes = {
-  images: PropTypes.arrayOf(PropTypes.shape({
-    node: PropTypes.shape(carouselImageProps),
-  })).isRequired,
+  images: PropTypes.arrayOf(
+    PropTypes.shape({
+      node: PropTypes.shape(carouselImageProps),
+    })
+  ).isRequired,
 };
 
 export default Carousel;
